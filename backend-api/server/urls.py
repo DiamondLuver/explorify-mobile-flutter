52add7af130af5f016f5a2151d018d2ad408109d
DiamondLuver <112000019+DiamondLuver@users.noreply.github.com>
2024-05-27 00:17:14 +0700

    Update urls and admin site name

--- backend-api/server/urls.py
+++ backend-api/server/urls.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-from django.urls import path
-from django.urls.conf import include
-from todo import urls
-
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
-    path('api/v1/', include(urls)),
     path('admin/', admin.site.urls),
+    
+    # path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    
+    path('', include('internship.urls')),
+    path('', include('account.urls')),
 ]
+
+# if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_title = "Explorify site admin (DEV)"
+admin.site.site_header = "Explorify administration"
+admin.site.index_title = "Site administration"
