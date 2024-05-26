b52e1db3c1a863f3403928656d0026675e60d93c
DiamondLuver <112000019+DiamondLuver@users.noreply.github.com>
2024-05-27 00:18:50 +0700

    remove todo

--- backend-api/todo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.todos),
-    path('test/', views.test_api, name='test_api'),
-    path('todo/<int:pk>/', views.todo),
-]
