ee4207f01e199232f9915ad52e85b522f8f7730a
Heaklinh <heaklinh@gmail.com>
2024-05-29 14:04:16 +0700

    Add Icon

--- frontend_mobile/lib/common/image_strings.dart
+++ frontend_mobile/lib/common/image_strings.dart
@@ -1,10 +1,9 @@
-
-
 class AppImage {
   static const String welcomeImg = "assets/images/welcome_img.png";
-  static const String logo = "";
-  static const String google = "";
-  static const String facebook = "";
+  static const String upperStyle = "assets/images/upper_style.png";
+  static const String logo = "assets/images/app_logo.png";
+  static const String google = "assets/images/google.png";
+  static const String facebook = "assets/images/facebook.png";
   static const String successImg = "";
   static const String sendEmailImg = "";
   static const String verifyEmailImg = "";
