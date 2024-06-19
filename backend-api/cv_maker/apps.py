b38f735f14d2623ae6851f654cbce551c2ac160c
DiamondLuver <112000019+DiamondLuver@users.noreply.github.com>
2024-06-19 23:25:00 +0700

    Add script to handle auto insert to db data default

--- backend-api/cv_maker/apps.py
+++ backend-api/cv_maker/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
-
-
+from django.db.models.signals import post_migrate
 class CvMakerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cv_maker'
+    def ready(self):
+        from  cv_maker.preset_data import load_preset_data
+        post_migrate.connect(load_preset_data, sender=self)
