983b1ff2efe9fceab76b558316d76fbbe2c38b11
DiamondLuver <112000019+DiamondLuver@users.noreply.github.com>
2024-06-19 23:26:04 +0700

    modify serializers to handle cv data

--- backend-api/cv_maker/urls.py
+++ backend-api/cv_maker/urls.py
@@ -1,7 +1,10 @@
 # urls.py
 from django.urls import path
-from .views import CVFormAPIView
+from .views import *
 
 urlpatterns = [
-    path("cv-form-data/", CVFormAPIView.as_view(), name="cv-form-data"),  # use Post method to Generate CV & Get Method to get Data for CV
+    path(
+        "cv-form-data/", CVFormAPIView.as_view(), name="cv-form-data"
+    ),  # use Post method to Generate CV & Get Method to get Data for CV
+    
 ]
--- backend-api/cv_maker/views.py
+++ backend-api/cv_maker/views.py
@@ -1,62 +1,67 @@
-from rest_framework.response import Response
 from rest_framework import status
 from django.http import HttpResponse
 from django.template.loader import get_template
-import weasyprint
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings
 from rest_framework import generics, permissions
 from rest_framework.exceptions import ValidationError
 from account.models import User
-from .serializers import CVDataSerializer, ImageUploadSerializer
+from .serializers import *
 from weasyprint import HTML, CSS
-from .models import (
-    CV,
-)
-
+from rest_framework.permissions import IsAuthenticated
+from .models import *
+from django.http import HttpResponse
+from base.utils import  * # format of API requests
 
 class CVFormAPIView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
+
     def get(self, request):
         try:
             user = User.objects.get(user_id=request.user.user_id)
         except User.DoesNotExist:
-            return Response(
-                {"error": "User does not exist."}, status=status.HTTP_404_NOT_FOUND
+            return error_response(
+                message="User does not exist.", status=status.HTTP_404_NOT_FOUND
             )
 
         try:
             cv = CV.objects.get(user=user)
         except CV.DoesNotExist:
-            return Response(
-                {"error": "CV not found for the user."},
+            return error_response(
+                message="CV not found for the user.",
                 status=status.HTTP_404_NOT_FOUND,
             )
 
         serializer = CVDataSerializer(cv)
-        return Response(serializer.data)
+        return success_response(
+            message="Get data successfully", data=serializer.data, status_code=status.HTTP_200_OK
+        )
 
     def post(self, request):
         try:
             user = User.objects.get(pk=request.user.pk)
             cv = CV.objects.get(user=user)
         except User.DoesNotExist:
-            return Response(
-                {"error": "User not found"}, status=status.HTTP_404_NOT_FOUND
+            return error_response(
+                message="User not found", status=status.HTTP_404_NOT_FOUND
             )
         except CV.DoesNotExist:
-            return Response(
-                {"error": "CV not found for the user"}, status=status.HTTP_404_NOT_FOUND
+            return error_response(
+                message="CV not found for the user", status=status.HTTP_404_NOT_FOUND
             )
 
         serializer = ImageUploadSerializer(data=request.data)
         if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return error_response(
+                message=serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
 
         try:
             serializer.validate_image_file(serializer.validated_data["image_file"])
         except ValidationError as e:
-            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            return error_response(
+                message=str(e), status=status.HTTP_400_BAD_REQUEST
+            )
 
         try:
             image_file = serializer.validated_data["image_file"]
@@ -67,8 +72,8 @@ class CVFormAPIView(generics.GenericAPIView):
                 f"{request.scheme}://{request.get_host()}{image_url}"
             )
         except Exception as e:
-            return Response(
-                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            return error_response(
+                message=str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
 
         serializer_data = CVDataSerializer(cv)
@@ -86,8 +91,8 @@ class CVFormAPIView(generics.GenericAPIView):
                 ]
             )
         except Exception as e:
-            return Response(
-                {"error": f"Failed to generate PDF: {str(e)}"},
+            return error_response(
+                message= f"Failed to generate PDF: {str(e)}",
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
 
@@ -98,9 +103,10 @@ class CVFormAPIView(generics.GenericAPIView):
         try:
             fss.delete(file)
         except Exception as e:
-            return Response(
-                {"error": f"Failed to delete image file: {str(e)}"},
+            return error_response(
+                message= f"Failed to delete image file: {str(e)}",
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
 
         return response
+
