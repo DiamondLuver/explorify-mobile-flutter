e8d633fee2c7aab2c90d16b2fe221776211aa385
DiamondLuver <112000019+DiamondLuver@users.noreply.github.com>
2024-06-30 21:03:34 +0700

    add pagination class to use in other views

--- backend-api/base/utils.py
+++ backend-api/base/utils.py
@@ -6,15 +6,20 @@ from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 import smtplib
 import logging
+from rest_framework.pagination import PageNumberPagination
 
 logger = logging.getLogger(__name__)
 
+
 def send_otp_email(to_email, user_name, otp):
-    subject = 'Your OTP Code'
-    message = render_to_string('email/otp_email_template.html', {
-        'user_name': user_name,
-        'otp': otp,
-    })
+    subject = "Your OTP Code"
+    message = render_to_string(
+        "email/otp_email_template.html",
+        {
+            "user_name": user_name,
+            "otp": otp,
+        },
+    )
     email = EmailMessage(
         subject,
         message,
@@ -25,49 +30,72 @@ def send_otp_email(to_email, user_name, otp):
     try:
         email.send()
     except BadHeaderError:
-        logger.error('Invalid header found.')
+        logger.error("Invalid header found.")
     except smtplib.SMTPException as e:
-        logger.error(f'SMTP error occurred: {e}')
-        
-        
+        logger.error(f"SMTP error occurred: {e}")
+
+
 def send_registration_confirmation_email(user_email, user):
-    subject = 'Registration Confirmation'
-    html_message = render_to_string('email/company/registration_email.html', {'user': user})
-    plain_message = strip_tags(html_message) 
+    subject = "Registration Confirmation"
+    html_message = render_to_string(
+        "email/company/registration_email.html", {"user": user}
+    )
+    plain_message = strip_tags(html_message)
     from_email = settings.EMAIL_HOST_USER
     to_email = [user_email]
 
     send_mail(subject, plain_message, from_email, to_email, html_message=html_message)
 
+
 def send_approval_notification_email(company_name, contact_email):
-    subject = 'New Company Registration Approval Required'
-    html_message = render_to_string('email/admin/approval_notification.html', {'company_name': company_name, 'contact_email': contact_email})
-    plain_message = strip_tags(html_message)  # Strip HTML tags for the plain text message
+    subject = "New Company Registration Approval Required"
+    html_message = render_to_string(
+        "email/admin/approval_notification.html",
+        {"company_name": company_name, "contact_email": contact_email},
+    )
+    plain_message = strip_tags(
+        html_message
+    )  # Strip HTML tags for the plain text message
     from_email = settings.EMAIL_HOST_USER
-    to_email = ['sreypich.un@student.cadt.edu.kh']  
-    bcc = ['unsreypich999@gmail.com','heaklinh@gmail.com', 'chanmaliza.tann@student.cadt.edu.kh']
+    to_email = ["sreypich.un@student.cadt.edu.kh"]
+    bcc = [
+        "unsreypich999@gmail.com",
+        "heaklinh@gmail.com",
+        "chanmaliza.tann@student.cadt.edu.kh",
+    ]
 
     msg = EmailMessage(subject, html_message, from_email, to_email, bcc=bcc)
-    msg.content_subtype = 'html' 
+    msg.content_subtype = "html"
     msg.send()
-    
-    
+
 
 def api_response(status, code, message, data=None, error=None):
     response_data = {
-        'status': status,
-        'code': code,
-        'message': message,
-        'data': data,
-        'error': error,
+        "status": status,
+        "code": code,
+        "message": message,
+        "data": data,
+        "error": error,
     }
     return Response(response_data, status=code)
 
 
+class LargeResultsSetPagination(PageNumberPagination):
+    page_size = 1000
+    page_size_query_param = "page_size"
+    max_page_size = 10000
 
-    """
-    Unused function
-    """
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 1000
+
+"""
+Unused function
+"""
+
+
 def send_simple_email(to_email, subject, message):
     try:
         send_mail(
@@ -78,9 +106,10 @@ def send_simple_email(to_email, subject, message):
             fail_silently=False,
         )
     except BadHeaderError:
-        logger.error('Invalid header found.')
+        logger.error("Invalid header found.")
     except smtplib.SMTPException as e:
-        logger.error(f'SMTP error occurred: {e}')
+        logger.error(f"SMTP error occurred: {e}")
+
 
 def send_html_email(to_email, subject, message):
     email = EmailMessage(
@@ -93,9 +122,10 @@ def send_html_email(to_email, subject, message):
     try:
         email.send()
     except BadHeaderError:
-        logger.error('Invalid header found.')
+        logger.error("Invalid header found.")
     except smtplib.SMTPException as e:
-        logger.error(f'SMTP error occurred: {e}')
+        logger.error(f"SMTP error occurred: {e}")
+
 
 def success_response(message="Success", data=None, status_code=status.HTTP_200_OK):
     return Response(
@@ -108,14 +138,12 @@ def success_response(message="Success", data=None, status_code=status.HTTP_200_O
         status=status_code,
     )
 
+
 def error_response(message, data=None, status_code=status.HTTP_400_BAD_REQUEST):
     response_data = {
         "result": "Error",
         "status_code": status_code,
         "result_message": message,
-        "body": data
-        
+        "body": data,
     }
-    # if error:
-    #     response_data["error"] = error
     return Response(response_data, status=status_code)
